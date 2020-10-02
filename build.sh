8b5b48a403ea8b4fc4cc4c11a192afad8196d463
Nicolas Bonamy <nicolas@bonamy.fr>
2020-10-01 20:42:19 -0500

    build and install script

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ cd build/Xcode
 xcodebuild -workspace EnergyBar.xcworkspace -scheme EnergyBar -configuration Release
 app=$(find /Users/nbonamy/Library/Developer/Xcode/DerivedData/EnergyBar*//Build/Products/Release -name EnergyBar.app)
 killall EnergyBar
-echo $app
+rm -rf /Applications/EnergyBar.app
 cp -r $app /Applications/
 open /Applications/EnergyBar.app
