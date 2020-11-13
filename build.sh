48fdf51e1e53642acc3a225db9a7b511dea35c67
Nicolas Bonamy <nicolas@bonamy.fr>
2020-11-13 17:01:43 -0600

    build script fix

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 cd build/Xcode
 xcodebuild -workspace EnergyBar.xcworkspace -scheme EnergyBar -configuration Release
-app=$(find /Users/nbonamy/Library/Developer/Xcode/DerivedData/EnergyBar*//Build/Products/Release -name EnergyBar.app)
+app=$(find $HOME/Library/Developer/Xcode/DerivedData/EnergyBar*//Build/Products/Release -name EnergyBar.app)
 killall EnergyBar
 rm -rf /Applications/EnergyBar.app
 cp -r $app /Applications/
+sleep 1
 open /Applications/EnergyBar.app
